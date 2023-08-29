77405e400681e054345b39c68af5786624cad3f6
jonaspmeise <jonaspmeise@users.noreply.github.com>
2023-08-29 22:04:03 +0000

    deploy: 586e621d47de18d846a28a43d584a40e80189915

--- src/app/pipes/values.pipe.ts
+++
@@ -1,8 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({ name: 'values' })
-export class ValuesPipe implements PipeTransform {
-  transform = (value: any): any[] => {
-    return Object.values(value);
-  }
-}
